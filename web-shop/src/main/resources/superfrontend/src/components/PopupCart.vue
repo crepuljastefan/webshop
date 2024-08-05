0c419adcc2f06c6c24512f4839948f448a1590fa
Stefan Crepulja <stefan.crepulja@gmail.com>
2024-08-05 17:20:40 +0200

    Created components for account and cart

---
+++ web-shop/src/main/resources/superfrontend/src/components/PopupCart.vue
@@ -0,0 +1,12 @@
+<template>
+  <button class="top-12 left-32 absolute" @click="closeToggle">x</button>
+</template>
+<script>
+export default {
+  methods: {
+    closeToggle() {
+      this.$emit("close");
+    },
+  },
+};
+</script>
